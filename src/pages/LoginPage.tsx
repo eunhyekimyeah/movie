9a4b5499f7a2ffefa379b2decf603ed6ca91a4b3
KimEunHye <rladmsgp031219@naver.com>
2026-06-21 15:38:31 +0900

    test3

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = () => {
     console.log('로그인 시도:', { email, password });
   }
 
