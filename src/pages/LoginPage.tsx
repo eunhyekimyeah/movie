bf336a37b4c59b3c2507a1212d62f937c1ed5e9b
KimEunHye <rladmsgp031219@naver.com>
2026-06-21 15:33:01 +0900

    test2

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import { useNavigate } from '@tanstack/react-router'
 
 export default function LoginPage() {
-  const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
